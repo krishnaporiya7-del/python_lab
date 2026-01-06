b32005c405914ebf827514dc0b96d48bdf7d1872
krishnaporiya7-del <krishnaporiya7@gmail.com>
2026-01-06 09:21:31 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,17 @@
+"""a=2
+print(a)  
+b=20
+print(a-b)"""
+"""x=["apple,cherry,banana"]
+print(x)"""
+"""x=range(10)
+print(x)"""
+"""x=319.29106
+print(x)""" 
+"""a={"cherry","apple","banana"}
+print(a)"""
+"""x={"name": "John","age":36}
+print(x)"""
+a=True or False
+b=True
+print(b)
