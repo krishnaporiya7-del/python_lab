d126910d41d63b6abf7293c55e8ba410f6214ac6
krishnaporiya7-del <krishnaporiya7@gmail.com>
2026-01-11 22:16:37 +0530

    Add files via upload

---
+++ assigment.py
@@ -0,0 +1,52 @@
+"""x="Hello welcome to Atmiya university"
+print(x)"""
+
+"""a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print("sum =",a+b)"""
+
+"""num=int(input("Enter a number:"))
+if num % 2==0:
+    print("Even Number")
+else:
+    print("Odd Number")"""
+    
+"""year=int(input("Enter a year:"))
+if year % 4==0:
+    print("Leap Year")
+else:
+    print("Not a Leap Year")"""
+
+"""import math
+print("PI value=",math.pi)"""    
+
+"""PI=3.14
+print("Constatnt value=,PI")"""
+
+"""num=int(input("Enter a number:"))
+square=num*num
+print("squre=",square)"""
+
+"""r=float(input("Enter radius:"))
+pi=3.14
+area=pi*r*r
+print("Area of circle=",area)"""
+
+"""x=input("Enter anything:")
+print("Data type is:",type(x))"""
+
+"""import math
+num=float(input("Enter a number:"))
+print("Square root=",math.sqrt(num))
+print("Absolute value=",abs(num))"""
+
+"""a=int(input("Enter base:"))
+b=int(input("Enter power:"))
+print("Result=",a**b)"""
+
+num=int(input("Enter a number:"))
+if num>0:
+    print("Positive number")
+if num<0:
+    print("Nagative  number")
+        
