caaff2634a0127010454758d6cf63a3ac3ee8343
krishnaporiya7-del <krishnaporiya7@gmail.com>
2026-01-13 15:33:23 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,50 @@
+"""p=float(input("Enter principle:"))
+r=float(input("Enter rate:"))
+t=float(input("Enter time:"))
+si=(p*r*t)/100
+print("Simple interest=",si)"""
+
+
+"""a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+if a>b:
+    print("Maximum=",a)
+else:
+    print("Maximum=",b)"""   
+
+
+"""for i in range(1,6):
+    print(i)""" 
+
+
+"""s=input("Enter a string:") 
+print("Length=",len(s))""" 
+
+
+"""print("Welcome to python programming")"""
+
+
+"""s=input("Enter a string:")
+print("First character=",s[0])"""
+
+
+"""s=input("Enter a string:")
+print("Last character=",s[-1])"""
+
+
+"""n=int(input("Enter a number:"))
+if n>0:
+    print("Positive")
+else:
+    print("Negative")"""  
+
+
+"""a=int(input("Enter first number:")) 
+b=int(input("Enter second number:"))
+c=int(input("Enter thired number:")) 
+sum=a+b+c   
+print("sum=",sum)"""
+
+
+"""name=input("Enter your name:")
+print("Hello Krishna")"""
