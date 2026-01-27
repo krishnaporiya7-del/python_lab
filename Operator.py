551334552d3d908fde08fb20c4eb71e6b95aa869
krishnaporiya7-del <krishnaporiya7@gmail.com>
2026-01-27 09:17:49 +0530

    Add files via upload

---
+++ Operator.py
@@ -0,0 +1,47 @@
+#Arithmetic operator
+"""a=5
+b=10
+print(a+b)
+print(a-b)
+print(a/b)"""
+
+#Assigment operator
+"""x=10
+print(x)"""
+
+#Unary Minus
+"""a=15
+print(-a)"""
+
+#Relational operator
+"""p=10
+q=5
+print(p>q)"""
+
+#Logical operator
+"""x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x)"""
+
+#Boolean(Boolean values)
+"""is_pass=True
+print(is_pass)"""
+
+#Bitwise operator
+"""a=6
+b=4
+print(a&b)
+print(a/b)"""
+
+#Membership operator
+"""numbers=[1,2,3,4]
+print(2 in numbers)
+print(5 is not numbers)"""
+
+#Identify operator
+"""x=2
+y=3
+print(x is y)
+print(x is not y)"""
