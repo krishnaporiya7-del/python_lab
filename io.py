91c9b3428fb1df9f78b27e4e99083e59b25dda91
krishnaporiya7-del <krishnaporiya7@gmail.com>
2026-01-31 08:25:13 +0530

    Add files via upload

---
+++ io.py
@@ -0,0 +1,13 @@
+#input
+"""print("welcome to python programming")
+print("myself krishna poriya")"""
+
+#output
+"""name=input("Enter your name:")
+print("Hello",name)"""
+"""rollno=input("Enter your roll no:")
+print("Hello",rollno)"""
+
+#output
+price=float(input("Enter price:"))
+print("price entered:",price)
