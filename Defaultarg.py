d816b55fce98007bba38f1c1af23ab1a81ce8d68
krishnaporiya7-del <krishnaporiya7@gmail.com>
2026-03-25 22:34:32 +0530

    Add files via upload

---
+++ Defaultarg.py
@@ -0,0 +1,24 @@
+#Basic argument default
+"""def greet(name,msg="Good Morning"):
+    print("Hello",name,msg)
+greet("Asha")
+greet("Ravi","Good Evening")"""
+
+
+#Power function
+"""def power(num,exp=2):
+    return num**exp
+print(power(3))
+print(power(2))
+print(power(2,4))"""
+
+
+#multiple Default argument
+"""def student_info(name,age=18,course="BSC.IT"):
+    print("Name:",name)
+    print("Age:",age)
+    print("course:",course)
+student_info("Ravi")
+student_info("Seema",20)
+student_info("Amit",19,"BSC.IT")""" 
+
