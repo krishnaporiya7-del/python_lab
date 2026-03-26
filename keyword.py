89ad1ca1d49d418ed9be6ef70eefac44d2890dcd
krishnaporiya7-del <krishnaporiya7@gmail.com>
2026-03-26 18:55:30 +0530

    Add files via upload

---
+++ keyword.py
@@ -0,0 +1,29 @@
+#Basic Keyword Argument
+"""def student_info(Name,Age,City):
+    print("Name:",Name)
+    print("Age:",Age)
+    print("City:",City)
+student_info(Age=18,City="Rajkot",Name="Ravi")""" 
+
+
+#Mixing positinol and keyword
+"""def display(a,b,c):      
+    print("a =",a)
+    print("b =",b)
+    print("c =",c)
+    
+display(1,c=3,b=2)"""
+
+
+
+#using keyword argument
+#create functions of simple interest
+"""def simple_interest(p:float,r:int,t:float):
+    si=(p*r*t)
+    print("simple Interest:",si)
+simple_interest(p=10000,t=2,r=1.5)
+simple_interest(t=1.5,p=15000,r=2)"""
+
+
+
+
