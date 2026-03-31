051da05f259925bb56f2776ef5d0ebb08b5f9568
krishnaporiya7-del <krishnaporiya7@gmail.com>
2026-03-31 08:38:43 +0530

    Add files via upload

---
+++ write.py
@@ -0,0 +1,15 @@
+"""f=open("one.txt","w")
+f.write("Hello student\n")
+f.write("Welcome to file handling.\n")
+f.write("Leraning is fun!\n")
+f.close()"""
+
+
+"""f=open("one.txt","w")
+f.write("New content only.\n")
+f.close()"""
+
+
+f=open("one.txt","w")
+f.write("This line is added at the end.\n")
+f.close()
