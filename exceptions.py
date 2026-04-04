7e691412a6c20d62bef8c94678ee3c95779b81d4
krishnaporiya7-del <krishnaporiya7@gmail.com>
2026-04-04 09:05:01 +0530

    Add files via upload

---
+++ exceptions.py
@@ -0,0 +1,31 @@
+"""try:
+    a = int(input("enter a number:"))
+    b = int(input("enter another number:"))
+    result = number1 / number2
+
+except zeroDivisiononError:
+    print("You cannot divide by zero!")
+
+except valueError:
+    print("please enter a valid number!")     
+
+else:
+    print("Division successfull Result is:",result)
+
+finally:
+    print("This block always runs.")"""
+
+
+
+"""try:
+    my_list = [1,2,3]
+    print(my_list[1])
+    
+except IndexError:
+    print("Index is out of range!")
+
+else:
+    print("Element found successfully!")
+
+finally:
+    print("Program finished.")"""    
